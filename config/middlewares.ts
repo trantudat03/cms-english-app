eb82b768f05fae677fe5fbbad8b1690b192ffd9f
trantudat03 <101052063+trantudat03@users.noreply.github.com>
2026-02-09 18:05:53 +0700

    feat(upload): configure AWS S3 provider for Cloudflare R2 storage
    Add @strapi/provider-upload-aws-s3 dependency and configure upload plugin to use Cloudflare R2 as the storage backend. Update content security policy in middlewares to allow media assets from the R2 endpoint.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_PUBLIC_URL') ? env('R2_PUBLIC_URL').replace(/^https?:\/\//, '') : `https://${env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_PUBLIC_URL') ? env('R2_PUBLIC_URL').replace(/^https?:\/\//, '') : `https://${env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
